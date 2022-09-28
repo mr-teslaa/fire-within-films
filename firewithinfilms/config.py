45341766a80cd7a17bd55f026ea1df076c51e88f
muzahid-nayim <muzahidulislamnayim@gmail.com>
2022-09-28 08:25:48 +0600

    fixing some production level setup

--- firewithinfilms/config.py
+++ firewithinfilms/config.py
@@ -1,14 +1,26 @@
 import os
+from dotenv import find_dotenv
+from dotenv import load_dotenv
 
+load_dotenv(find_dotenv())
+
+production=True
+# production=False
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    # SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1/firewithinflims'
+
+    if production:
+        SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1/firewithinflims'
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+        
     # set optional bootswatch theme
     FLASK_ADMIN_SWATCH = 'cerulean'
-    MAIL_SERVER = 'premium249.web-hosting.com'
+
+    #   SETUP SMTP MAIL SERVER
+    MAIL_SERVER = os.getenv('EMAIL_SMTP_SERVER')
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'sayhi@hossainfoysal.com'        # os.environ.get('EMAIL_USERNAME')
-    MAIL_PASSWORD = 'hossainfoysal.emailaddress.sayhi'        # os.environ.get('EMAIL_PASSWORD')
+    MAIL_USERNAME = os.getenv('EMAIL_USERNAME')        
+    MAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
